65ae6b99bfb8c984bc61ce36918701ab73caa56d
lol520 <miaoge1@qq.com>
2014-11-21 13:06:00 +0800

    11月21日更新
    听说这个走砍有菜单，就把他简单汉化了

---
+++ 【奶瓶汉化】LX走砍独立版/LxOrbwalker Standalone/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using LX_Orbwalker;
+
+namespace LxOrbwalker_Standalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+
+        public static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.PrintChat("<font color='#FF0000'>濂剁摱姹夊寲-LX璧扮爫</font> 杞藉叆鎴愬姛锛岃嫳闆勮仈鐩熺矇涓濈珯锛圠OL520.cc鍔╂偍娓告垙鎰夊揩锛侊級 - <font color='#5882FA'>濂剁摱姹夊寲</font>");
+            var menu = new Menu("濂剁摱姹夊寲-LX璧扮爫", "my_mainmenu", true);
+            var orbwalkerMenu = new Menu("濂剁摱姹夊寲-LX璧扮爫", "my_Orbwalker");
+            LXOrbwalker.AddToMenu(orbwalkerMenu);
+            menu.AddSubMenu(orbwalkerMenu);
+            menu.AddToMainMenu();
+        }
+
+    }
+}
